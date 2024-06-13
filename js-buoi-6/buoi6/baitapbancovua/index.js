79111d2e4fb05bd8c3fb6e31f0ff22648fe65b01
boithanh <boithanh01694@gmail.com>
2024-06-13 19:45:57 +0700

    backup code js buoi 7

--- js-buoi-6/buoi6/baitapbancovua/index.js
+++ js-buoi-6/buoi6/baitapbancovua/index.js
@@ -1,49 +1,55 @@
 //Yeeu cầu bài tập bàn cờ vua: thực hiện viết một chức năng (function) xử lí tạo ra layout một bàn cờ vua với 8 cột và 8 dòng
 // saukhi hoàn thành chức năng , xử lý dữ liệu động cho function để có thể tạo giao diện phù hợp với số cột và số dofngddc cung cấp từ người dùng
 
-let baiTap = document.getElementById("baiTapBanCoVua");
+// function renderBanCoVua(soCot = 0, soDong = 0) {
 
-// function taoO() {
-
-// }
-function renderBanCoVua(soCot = 0, soDong = 0) {
-
-    let oTrang = "<div class='oTrang'></div";
-    let oDen = "<div class='oDen'></div";
-    let dong = "";
-
-    for (let z = 1; z <= 8; z++) {
-        for (let i = 1; i <= 8; i++) {
-            //Taạo cột cho dòng lẻ
-            if (z % 2 == 1) {
-                if (i % 2 == 1) {
-                    dong += oTrang;
-                }
-                else {
-                    dong += oDen;
-                }
-                //  i%2==1?(dong+=oTrang):(dong+=oDen);
+let oTrang = "<div class='oTrang'></div>"
+let oDen = "<div class='oDen'></div>"
+let dong = "";
+//Tạo ra các dòng chẵn và lẻ theo điều kiện If bên trong  
+for (let z = 1; z <= 8; z++) {
+    //Tạo cho nó có các dòng dựa trên dòng lẻ hoặc dòng chẫn
+    for (let i = 1; i <= 8; i++) {
+        //Tao cột chẵn và lẻ
+        if (z % 2 == 1) {
+            //Tạo cột cho dòng lẻ
+            if (i % 2 == 1) {
+                dong += oTrang;
             }
-            // Tạo cột cho dòng chẵn
             else {
-                if (i % 2 == 1) {
-                    dong += oDen;
-                }
-                else {
-                    dong += oTrang;
-                }
-                i % 2 == 1 ? (dong += oDen) : (dong += oTrang);
+                dong += oDen
             }
-            baiTap.innerHTML += dong;
+        }    //  i%2==1?(dong+=oTrang):(dong+=oDen);
+        // }
+        // Tạo cột cho dòng chẵn
+        else {
+            if (i % 2 == 0) {
+                dong += oDen;
+            }
+            else {
+                dong += oTrang;
+            }
+
         }
+
+        // i % 2 == 1 ? (dong += oDen) : (dong += oTrang);
+        // }
+
+        // baiTap.innerHTML += dong;
+        // }
+        // }
+        //Thực hiện thêm HTML lên giao diện
+        // ketQua += `<div>${dong}</div>`;
+        // return ketQua;
+
     }
-    //Thực hiện thêm HTML lên giao diện
-    ketQua += `<div>${dong}</div>`;
-    return ketQua;
+
 }
-// document.getElementById("baiTapBanCoVua").innerHTML += `<div>${dong}</div>`;
+document.getElementById("baiTapBanCoVua").innerHTML += `<div>${dong}</div>`;
 
 
 
 
-document.getElementById("baiTapBanCoVua").innerHTML += renderBanCoVua();
+
+// document.getElementById("baiTapBanCoVua").innerHTML += renderBanCoVua();
+
--- js_buoi_7/array/index.js
+++ js_buoi_7/array/index.js
@@ -38,7 +38,6 @@ document.querySelector(".btn-success").onclick = () => {
         }
     }
     console.log(dem);
-
 };
 
 //Phương thức indexOf và lastIndexOF
@@ -62,12 +61,148 @@ for (let i = 0; i < danhSachSinhVien; i++) {
 console.log(vitriNguyen);
 
 
-
 let viTriVan = -1;
-for (let i = doDaimang - 1; i >= 0; i--) {
+for (let i = doDaiMang - 1; i >= 0; i--) {
     if (danhSachSinhVien[i] == "Vân") {
         viTriVan = i;
         break;
     }
 }
 console.log(viTriVan);
+
+
+let arrString = ["h", "e", "l", "l", "o"];
+let doDaiM = arrString.length;
+// let daonguoc = [];
+// console.log(arrString.reverse());
+// Yêu cầu: đảo ngược độ dài mảng bằng vòng lặp
+
+// for (let i = ddm - 1; i >= 0; i--) {
+//     daonguoc.push(daonguoc[i]);
+
+// }
+// console.log(daonguoc);
+
+for (let i = doDaiM; i >= 0; i--) {
+    // let lastItem = arrString.pop();
+    // arrString.unshift(lastItem);
+    // console.log(arrString[i]);
+    arrString[doDaiM - 1] = arrString[i];
+    console.log(arrString[i]);
+}
+
+
+let arrFood = ["Mì Xào", "Mì gõ", "Bánh canh", "cơm tấm", "Bánh cuốn", "Bún bò", "Sủi cảo"];
+
+// console.log(arrFood.soft().reverse());
+
+let arrSode = [1, 9, 11, 34, 45];
+console.log(
+    arrSode.sort(function (a, b) {
+        return b - a;
+    })
+);
+
+console.log(arrSode.sort((a, b) => b - a));
+
+
+//Thực hiện chuyện đổi đảo ngược chuỗi hello => olleh
+let string = "hello";
+// console.log(reverseString(string));
+let arrStringConvert = string.split("").reverse().join("");
+// function reverseString(string) {
+//     return .
+// }
+
+console.log(arrStringConvert);
+
+// PHUONG THỨC SLIE GIÚP CẮT RA THÀNH 1 MẢNG MỚI HOẶC CẮT BBOR 1 PHẦN TỬ BẤT KỲ HOẶC THÊM VÀO 1 PHẦN TỬ BẤT KÝ Ở VỊ TRÍ CHIWR ĐỊNH
+
+let arrHocVien = ["Toàn", "Vân", "Đông", "Kiên", "Mai", "Hoàng"];
+
+let newArrHocVien = arrHocVien.slice(0, 4);
+console.log(newArrHocVien);
+arrHocVien.splice(2, 1);
+console.log(arrHocVien
+);
+arrHocVien.splice(6, 0, "sang", "khải");
+console.log(arrHocVien);
+
+let arrSoXo = [1, 98, 4];
+let arrSoLo = [5, 1, 56];
+let newArrNumber = arrSoXo.concat(arrSoLo);
+console.log(newArrNumber);
+
+//Yêu cầu: KH cung cấp 1 mảng các số, trong mảng sẽ có các phần tử bị lặp, loại bỏ ra các phàn tử trung lập
+
+let arrNumberCustom = [1, 45, 21, 3, 67, 45, 53, 98, 21, 67, 1, 143, 678, 67];
+// cách 1:
+let doDai = arrNumberCustom.length;
+let kiemTra = "";
+for (let i = 0; i <= doDai; i++) {
+    let phanTuNamViTriIndex = arrNumberCustom[i];
+    for (let z = i + 1; z <= doDai; z++) {
+        {
+            if (arrNumberCustom[z] == phanTuNamViTriIndex) {
+                //Phần tử nằm ở cị trí z chính là phần tử bị lặp
+                arrNumberCustom.splice(z, 1);
+            }
+        }
+    }
+}
+console.log(arrNumberCustom);
+
+//Cách nâng cao (Ko bị ảnh hưởng hiệu suất vì cách trên chạy 2 vòng lập, nếu Data lướn sẽ rất chậm )
+
+// let object = {};
+// for (let i = 0; i < arrNumberCustom.length; i++) {
+//     if (object[arrNumberCustom[i]] != arrNumberCustom[i]) {
+//         object[arrNumberCustom[i]] = arrNumberCustom[i];
+//     }
+//     else {
+//         arrNumberCustom.splice(i, 1);
+//     }
+// }
+
+
+
+// console.log(arrNumberCustom);
+
+
+let arrMonAnThai = ["Xôi Xoài", "Tom Yum", "Lẩu Thái", "Sầu riêng", "cơm dứa"];
+//for in (Tạo ra một biến chứa vị trí index của các phần tử)
+// for (let index in arrMonAnThai) {
+//     console.log(arrMonAnThai[index]);
+// }
+for (let item of arrMonAnThai) {
+    console.log(item);
+}
+
+
+arrMonAnThai.forEach(function (item, index) {
+    console.log(index);
+    console.log(item);
+});
+
+let soTien = [15000, 100000, 35000, 87000, 45000];
+
+let newArrSoTien = soTien.filter(function (item, index) {
+    return item >= 87000;
+});
+console.log(newArrSoTien);
+
+//map
+let newArrMonAnThai = arrMonAnThai.map(function (item, index) {
+    return "Hello " + item;
+}
+)
+
+let arrInput = document.querySelectorAll("#baiTap2 input #baiTap2 button");
+console.log(arrInput);
+//Sử dụng vòng lặp để duyệt mảng
+for (let dom of arrInput) {
+    console.log(dom.id);
+}
+
+let arrButtonDark = document.getElementsByClassName("btn-dark");
+
